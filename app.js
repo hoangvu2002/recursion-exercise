79b01a063c8b079b08ad78cf86184b51db2c9abc
pdhoangvu <pdhoangvu@gmail.com>
2023-10-05 13:57:23 +0700

    Add the first iteration solution

---
+++ app.js
@@ -0,0 +1,15 @@
+function fibs(n) {
+    const result = [0,1];
+    if (n===1) {
+        return [0];
+    } else if (n===2) {
+        return result;
+    } else {
+        for (let i=2; i<n; i++) {
+            result.push(result[i-1]+result[i-2]);
+        }
+    };
+    return result;
+}
+
+console.log(fibs(8));
