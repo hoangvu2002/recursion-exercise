7a89538db261e447dbb99f1a410b2a1bd4c0af5f
pdhoangvu <pdhoangvu@gmail.com>
2023-10-06 10:34:13 +0700

    Add recursive solution

--- app.js
+++ app.js
@@ -13,3 +13,20 @@ function fibs(n) {
 }
 
 console.log(fibs(8));
+
+function fibsRec(n) {
+    if (n <= 0) {
+      return [];
+    } else if (n === 1) {
+      return [0];
+    } else if (n === 2) {
+      return [0, 1];
+    } else {
+      const fibs = fibsRec(n - 1);
+      fibs.push(fibs[fibs.length - 1] + fibs[fibs.length - 2]);
+      return fibs;
+    }
+  }
+  
+  console.log(fibsRec(8)); // [0, 1, 1, 2, 3, 5, 8, 13]
+  
